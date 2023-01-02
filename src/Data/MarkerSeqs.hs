ab8b4c3449cffc19fa6ab3ba2cd35ac2f5e362dd
Oleksandr-Zhabenko <oleksandr.zhabenko@yahoo.com>
2023-01-02 21:40:09 +0200

    Updated the Data.MarkerSeqs module

--- src/Data/MarkerSeqs.hs
+++ src/Data/MarkerSeqs.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE NoImplicitPrelude, MagicHash #-}
+{-# LANGUAGE NoImplicitPrelude, MagicHash, BangPatterns #-}
 
 -- |
 -- Module      :  Data.MarkerSeqs
--- Copyright   :  (c) Oleksandr Zhabenko 2022
+-- Copyright   :  (c) Oleksandr Zhabenko 2022-2023
 -- License     :  MIT
 -- Stability   :  Experimental
 -- Maintainer  :  oleksandr.zhabenko@yahoo.com
@@ -20,14 +20,14 @@ import Data.Bits
 import Numeric (showIntAtBase,showInt)
 import Data.Foldable (Foldable)
 import GHC.Int
---import qualified Data.Heap as H
+import Data.Maybe (mapMaybe, catMaybes)
 
 showBin :: Int -> [Char]
 showBin x = '0':'b':showIntAtBase 2 (head . flip showInt "") x ""
 {-# INLINE showBin #-}
 
-unionCount :: (Bits a) => a -> a -> Int
-unionCount x = popCount . (.&.) x
+unionCount :: (Bits a) => a -> a -> Integer
+unionCount x = toInteger . popCount . (.&.) x
 {-# INLINE unionCount #-}
 
 countWeightsQs :: (Foldable t) => [t a -> Int] -> [t a] -> [[Int]]
@@ -73,15 +73,49 @@ splitF n ys =
         where f' n ks@(_:_) = let (ts,ws) = splitAt n ks in ts : f' n ws
               f' _ _ = []
 
-getIndices2 
+{- getIndices2 
   :: Ord a => Int8 
   -> [Int8] 
   -> [a] 
   -> [[[Int8]]]
-getIndices2 groupLength ks xs = filter (not. null) . map (filter (not . null). map (idList ks) . filter (not . null) . g [groupLength-1,groupLength-2..] . sort . 
+getIndices2 groupLength ks xs = map (filter (not . null). map (idList ws) . g [groupLength-1,groupLength-2..] . sort . 
    zipWith S2 [groupLength-1,groupLength-2..]) . splitF (fromIntegral groupLength) $ xs
-     where g (q:qs) xs@(x:ys) = let (js,rs) = span (== x) ys in map (\(S2 k y) -> As3 k q y) (x:js) : g qs rs
+     where !ws =  sortBy (\x y -> compare y x) . filter (>= 0) $ ks
+           g (q:qs) xs@(x:ys) = let (js,rs) = span (== x) ys in map (\(S2 k y) -> As3 k q y) (x:js) : g qs rs
            g _ _ = []
+-}
+
+getHashes2 
+  :: Ord a => Int8 
+  -> [Int8] 
+  -> [a] 
+  -> [[Integer]]
+getHashes2 groupLength ks xs = map (map toNum . filter (not . null) . map (idList ws) .  g [groupLength-1,groupLength-2..] . sort . 
+   zipWith S2 [groupLength-1,groupLength-2..]) . splitF (fromIntegral groupLength) $ xs
+     where !ws = sortBy (\x y -> compare y x) . filter (>= 0) $ ks
+           g (q:qs) xs@(x:ys) = let (js,rs) = span (== x) ys in map (\(S2 k y) -> As3 k q y) (x:js) : g qs rs
+           g _ _ = []
+
+countHashesPrioritized tss@(ts:vs:xss) = zipWith unionCount ts vs : countHashesPrioritized (vs:xss)
+countHashesPrioritized _ = []
+
+count1Hashes 
+  :: Ord a => Int8
+  -> [Int8]
+  -> [a]
+  -> Integer
+count1Hashes groupLength ks = sum . map createNewHash . countHashesPrioritized . getHashes2 groupLength ks 
+{-# INLINE count1Hashes #-}
+
+createNewHash :: [Integer] -> Integer
+createNewHash (x1:x2:x3:x4:x5:x6:x7:_) = sum [shiftL x1 120, shiftL x2 100, shiftL x3 80, shiftL x4 60, shiftL x5 40, shiftL x6 20, x7]
+createNewHash (x1:x2:x3:x4:x5:x6:_) = sum [shiftL x1 120, shiftL x2 100, shiftL x3 80, shiftL x4 60, shiftL x5 40, shiftL x6 20]
+createNewHash (x1:x2:x3:x4:x5:_) = sum [shiftL x1 120, shiftL x2 100, shiftL x3 80, shiftL x4 60, shiftL x5 40]
+createNewHash (x1:x2:x3:x4:_) = sum [shiftL x1 120, shiftL x2 100, shiftL x3 80, shiftL x4 60]
+createNewHash (x1:x2:x3:_) = sum [shiftL x1 120, shiftL x2 100, shiftL x3 80]
+createNewHash (x1:x2:_) = sum [shiftL x1 120, shiftL x2 100]
+createNewHash (x1:_) = shiftL x1 120
+createNewHash _ = 0
 
 idList :: Eq a => [Int8] -> [ASort3 a] -> [Int8]
 idList orDs ys = map (\(As3 k _ _) -> k) . filter (\(As3 _ n _) -> n `elem` orDs) $ ys
@@ -91,4 +125,5 @@ toNum xs = foldl' setBit 0 . map (\(I8# k) -> I# k) $ xs
 
 toNum2 :: [Int8] -> Integer
 toNum2 xs = (sum . map (shiftL 1 . (\(I8# k) -> I# k)) $ xs)::Integer
+
 
